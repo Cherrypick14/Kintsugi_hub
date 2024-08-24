7332b321319721100e8e4f8085ce3cf4c5897a01
Cherrypick14 <cherrypicklet@gmail.com>
2024-08-24 15:24:30 +0300

    add new files

--- src/kintsugi_hub_frontend/src/App.jsx
+++ src/kintsugi_hub_frontend/src/App.jsx
@@ -1,30 +1,18 @@
 import { useState } from 'react';
 import { kintsugi_hub_backend } from 'declarations/kintsugi_hub_backend';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    kintsugi_hub_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <Router>
+      <Routes>
+        {/* Add more routes for other pages */}
+        <Route path='/' element ={<Home />} ></Route>
+      </Routes>
+    </Router>
+    
   );
 }
 
